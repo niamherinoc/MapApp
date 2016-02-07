610614f4b5c9a2a07e006269fead489a685d5a4c
Niamh O'Conchubhair <xxbibblezxx@hotmail.com>
2016-02-07 11:35:40 +0000

    add node, express, mongo project

---
+++ TravelApp/routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var Converter = require("csvtojson").Converter;
+var converter = new Converter({});
+var _ = require('underscore');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //Converter Class
+  converter.fromFile("../Countries-Continents.csv",function(err,result){
+    var groupedCountries = _.chain(result)
+    .groupBy('Continent')
+    .map(function(value, key) {
+        return {
+            continent: key,
+            countries: _.pluck(value, 'Country')
+        }
+    })
+    .value();
+
+    res.render('index', {
+      title: 'Express',
+      data: groupedCountries
+    });
+  });
+});
+
+module.exports = router;
---
+++ TravelApp/routes/users.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* GET Userlist page. */
+router.get('/map', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('map', {
+            "info" : docs
+        });
+    });
+});
+
+
+module.exports = router;
